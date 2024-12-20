b05156c53a8a37c631159a1ef9ce7725e0ec1f54
sejeong <ygn06024@naver.com>
2024-12-20 14:29:50 +0900

    로그인창-이메일 유효성검사 및 에러메세지

--- login/login.js
+++ login/login.js
@@ -14,7 +14,7 @@ emailBox.addEventListener("focusout", (event)=> {
     }
   });
 
-pwdBox.addEventListener("mouseout", (event)=> {
+pwdBox.addEventListener("focusout", (event)=> {
   if(!event.target.value) {
     event.target.style.border = "1px solid red";
     errorMsgPwd.style.display = "block";
@@ -24,8 +24,19 @@ pwdBox.addEventListener("mouseout", (event)=> {
   }
 });
 
-//이메일 유효성 검사사
-const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-if (!emailRegex.test(emailBox)) {
-  document.querySelector
+//이메일 유효성 검사
+const errorMsgEmail_T =document.querySelector("#error-message-email-type");
+const emailRegex = /^[a-z0-9._%+-]{1,}@[a-z0-9-]+(\.[a-z0-9-]+)*\.[a-z]{2,}$/
+
+function emailcheck(emailBox) {
+  return /^[a-z0-9._%+-]{1,}@[a-z0-9-]+(\.[a-z0-9-]+)*\.[a-z]{2,}$/.test(emailBox);
 }
+
+emailBox.addEventListener("input", (e) => {
+  if (!e.target.value.length) {
+    errorMsgEmail_T.style.display = "none";
+    return;
+  }
+  if (emailcheck(e.target.value)) errorMsgEmail_T.style.display = "none";
+  else errorMsgEmail_T.style.display = "block";
+})
