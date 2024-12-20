035eaddf585c6202064714f29095e4226f85d6e1
HoeunRyu <hoeun9565@gmail.com>
2024-12-20 14:19:12 +0900

    [login]feat/오류 메시지 모달 구현

--- js/login.js
+++ js/login.js
@@ -1,5 +1,8 @@
+import {USER_DATA} from "./userData.js";
+
 const emailInput = document.querySelector("#email_input");
 const pwInput = document.querySelector("#pw_input");
+
 const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 const pwRegex = /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,16}$/;
 
@@ -7,17 +10,22 @@ const loginBtn = document.querySelector("#login_btn");
 
 const visibleBtn = document.querySelectorAll(".visible_btn");
 
+const modal = document.querySelector("#modal-bg");
+const modalMsg = document.querySelector("#modal-msg");
+const closeBtn = document.querySelector("#close-btn");
+
 const msgList = {
     emailInputErrorMsg: "이메일을 입력해주세요.",
     emailValidErrorMsg: "잘못된 이메일 형식입니다.",
     pwInputErrorMsg: "비밀번호를 입력해주세요.",
     pwLengthErrorMsg: "비밀번호를 8자 이상 입력해주세요.",
-    pwValidErrorMsg: "잘못된 비밀번호 형식입니다."
+    pwValidErrorMsg: "잘못된 비밀번호 형식입니다.",
+    loginErrorModalMsg: "이메일 또는 비밀번호가 잘못 되었습니다."
 }
 
 loginBtn.disabled = true;
 
-//에러 메세지 표시 함수
+//에러 메시시지 표시 함수
 const showError = (input, msg) => {
     clearError(input);
 
@@ -30,7 +38,7 @@ const showError = (input, msg) => {
     input.parentElement.insertAdjacentElement("afterend", errorMsg);
 }
 
-//에러 메세지 제거 함수
+//에러 메시시지 제거 함수
 const clearError = (input) => {
     const errorMsg = input.parentElement.nextElementSibling;
     input.classList.remove("show_error");
@@ -41,21 +49,55 @@ const clearError = (input) => {
 };
 
 //로그인 버튼 활성화 함수
-const activateLoginBtn = () => {
-    //이메일, 비밀번호 모두 유효한지 검사
+const toggleLoginBtnState = () => {
     const isEmailValid = emailRegex.test(emailInput.value);
     const isPwValid = pwRegex.test(pwInput.value);
 
-    //모두 유효하면 활성화
+    //이메일, 비밀번호 모두 유효하면 활성화
     if (isEmailValid && isPwValid) {
         loginBtn.disabled = false;
         loginBtn.classList.add("activate");
     }
+    //로그인 버튼 비활성화
     else {
         loginBtn.disabled = true;
         loginBtn.classList.remove("activate");
     }
 }
+
+//모달 팝업 표시 함수
+const showModal = (msg) => {
+    modalMsg.innerHTML = msg;
+    modal.classList.add("show")
+}
+
+//모달 팝업 제거 함수
+const closeModal = () => {
+    modal.classList.remove("show");
+}
+
+closeBtn.addEventListener("click", closeModal);
+
+
+//로그인 성공/실패
+const loginErrorModal = () => {
+    const userData = USER_DATA.find((element) => element.email === emailInput.value);
+    console.log(userData)
+
+    const isEmailNotFound = !userData;
+    const isPwIncorrect = userData && userData.password !==pwInput.value;
+
+    //데이터 일치하지 않으면 로그인 실패, 모달 팝업
+    if (isEmailNotFound || isPwIncorrect) {
+        showModal(msgList.loginErrorModalMsg);
+    }
+    //일치하면 로그인 성공, 페이지 이동
+    else {
+        window.location.href = "../items.html";
+    }
+}
+
+loginBtn.addEventListener("click", loginErrorModal);
 
 //이메일 유효성 검사 함수
 const validEmail = (e) => {
@@ -73,7 +115,7 @@ const validEmail = (e) => {
         clearError(email);
     }
 
-    activateLoginBtn();
+    toggleLoginBtnState();
 }
 
 //비밀번호 유효성 검사 함수
@@ -96,7 +138,7 @@ const validPw = (e) => {
         clearError(pw);
     }
 
-    activateLoginBtn();
+    toggleLoginBtnState();
 }
 
 emailInput.addEventListener("blur", validEmail);
