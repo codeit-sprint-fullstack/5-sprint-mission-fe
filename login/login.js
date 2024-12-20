5883ed674f7f84dab5507951a282b71c46b08650
sejeong <ygn06024@naver.com>
2024-12-20 11:52:10 +0900

    display none을 class hide로 합침

--- login/login.js
+++ login/login.js
@@ -1,16 +1,16 @@
-//이메일input에서 focus out할 때, 값이 없을 경우 input 빨강테두리+에러메세지
+//input에서 focus out할 때, 값이 없을 경우 input 빨강테두리+에러메세지
 const emailBox = document.querySelector("#uid");
 const pwdBox = document.querySelector("#password");
-const errorMsgEmail = document.querySelector("#error-message-email");
+const errorMsgEmail_N = document.querySelector("#error-message-email-none");
 const errorMsgPwd = document.querySelector("#error-message-pwd");
 
 emailBox.addEventListener("focusout", (event)=> {
   if(!event.target.value) {
     event.target.style.border = "1px solid red";
-    errorMsgEmail.style.display = "block";
+    errorMsgEmail_N.style.display = "block";
     } else {
       event.target.style.border = "none";
-      errorMsgEmail.style.display = "none";
+      errorMsgEmail_N.style.display = "none";
     }
   });
 
@@ -22,4 +22,10 @@ pwdBox.addEventListener("mouseout", (event)=> {
     event.target.style.border = "none";
     errorMsgPwd.style.display = "none";
   }
-})
+});
+
+//이메일 유효성 검사사
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+if (!emailRegex.test(emailBox)) {
+  document.querySelector
+}
