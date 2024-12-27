cba9d99918890fe2209ccbcbd56a5b55d1802844
HoeunRyu <hoeun9565@gmail.com>
2024-12-27 16:45:23 +0900

    fix/url 수정, 파라미터 수정, 주석 추가, 디테일 작업

--- api/articleService.js
+++ api/articleService.js
@@ -1,88 +1,115 @@
-export const articleUrl = new URL("https://sprint-mission-api.vercel.app/articles");
+const articleUrl = "https://sprint-mission-api.vercel.app/articles";
 
-//게시글 목록 조회
-export const getArticleList = async ({page, pageSize, keyword}) => {
-    try {
-        articleUrl.searchParams.append("page", page);
-        articleUrl.searchParams.append("pageSize", pageSize);
-        articleUrl.searchParams.append("keyword", keyword);
+/** 게시글 목록 조회
+ * @param params 객체 {page: integer(1), pageSize: integer(100), keyword: "string"}
+ * @descrtipion page, pageSize, keyword -> query
+ * @returns 게시글 객체 목록 배열 반환
+ */
+const getArticleList = (params) => {
+    const getArticleUrl = new URL(articleUrl);
 
-        const response = fetch(articleUrl)
-        .then((response) => response.json())
-        .then((data) => data);
-        return response;
-
-    } catch (err) {
-        console.log("Err: ", err);
+    for (const [key, value] of Object.entries(params)) {
+        getArticleUrl.searchParams.append(key, value);
     }
+
+    const response = fetch(getArticleUrl)
+    .then((response) => response.json())
+    .then((data) => data)
+    .catch((err) => console.log("error: ", err))
+    
+    return response;
 }
 
-//게시글 상세 조회
-export const getArticle = async (id) => {
-    try {
-        const response = fetch((articleUrl + `/${id}`))
-        .then((response) => response.json())
-        .then((data) => data);
-        return response;
-
-    } catch (err) {
-        console.log("Err: ", err);
-    }
+/** 게시글 상세 조회
+ * @param id 게시글 ID: integer
+ * @description id -> path
+ * @returns response 객체 {id, title, content, image, likeCount}
+ */
+const getArticle = (id) => {
+    const response = fetch((articleUrl + `/${id}`))
+    .then((response) => {
+        if (!response.ok) {
+            throw new Error(`${response.status}: 상품을 찾을 수 없습니다.`);
+        }
+        return response.json()
+    })
+    .then((data) => data)
+    .catch((err) => console.log("error: ", err))
+        
+    return response;
 }
 
-//게시글 등록
-export const createArticle = async ({title, content, image}) => {
-    try {
-        const response = fetch(articleUrl, {
-            method: "POST",
-            headers: {"Content-Type": "application/json",},
-            body: JSON.stringify({
-                title,
-                content,
-                image
-            })
+/** 게시글 등록
+ * @param params 객체 {title: "string", content: "string", image: "string"}
+ * @description title, content, image -> body
+ * @returns 등록된 객체 {id, title, content, image, likeCount}
+ */
+const createArticle = (params) => {
+    const response = fetch(articleUrl, {
+        method: "POST",
+        headers: {"Content-Type": "application/json",},
+        body: JSON.stringify({
+            title: params.title,
+            content: params.content,
+            image: params.image
         })
-        .then((response) => response.json())
-        .then((data) => data);
-        return response;
-
-    } catch (err) {
-        console.log("Err: ", err);
-    }
+    })
+    .then((response) => {
+        if (!response.ok) {
+            throw new Error(`${response.status}: 유효성 검사 오류`);
+        }
+        return response.json()
+    })
+    .then((data) => data)
+    .catch((err) => console.log("error: ", err))
+        
+    return response;
 }
 
-//게시글 수정
-export const patchArticle = async ({id, title, content, image}) => {
-    try {
-        const response = fetch((articleUrl + `/${id}`), {
-            method: "PATCH",
-            headers: {"Content-Type": "application/json",},
-            body: JSON.stringify({
-                title,
-                content,
-                image
-            })
+/** 게시글 수정
+ * @param params 객체 {id: integer, title: "string", content: "string", image: "string"}
+ * @description id -> path / title, content, image -> body
+ * @returns 수정된 객체 {id, title, content, image, likeCount}
+ */
+const patchArticle = (params) => {
+    const response = fetch(articleUrl + `/${params.id}`, {
+        method: "PATCH",
+        headers: {"Content-Type": "application/json",},
+        body: JSON.stringify({
+            title: params.title,
+            content: params.content,
+            image: params.image
         })
-        .then((response) => response.json())
-        .then((data) => data);
-        return response;
+    })
+    .then((response) => {
+        if (!response.ok) {
+            throw new Error(`${response.status}: 수정할 상품을 찾을 수 없습니다.`);
+        }
+        return response.json()
+    })
+    .then((data) => data)
+    .catch((err) => console.log("error: ", err))
 
-    } catch (err) {
-        console.log("Err: ", err);
-    }
+    return response;
 }
 
-//게시글 삭제
-export const deleteArticle = async (id) => {
-    try {
-        const response = fetch((articleUrl + `/${id}`), {
-            method: "DELETE",
-            headers: {"Content-Type": "application/json",}
-        })
-
-        return response;
-
-    } catch (err) {
-        console.log("Err: ", err);
-    }
+/** 게시글 삭제
+ * @param id 게시글 ID: integer
+ * @description id -> path
+ * @returns 상품 삭제 성공 시 성공 메시지 콘솔에 출력
+ */
+const deleteArticle = (id) => {
+    fetch((articleUrl + `/${id}`), {
+        method: "DELETE",
+        headers: {"Content-Type": "application/json",}
+    })
+    .then((response) => {
+        if (!response.ok) {
+            throw new Error(`${response.status}: 삭제할 상품을 찾을 수 없습니다.`);
+        }
+        console.log("상품을 삭제했습니다.");
+    })
+    .catch((err) => console.log("error: ", err))
 }
+
+export { articleUrl, getArticleList, getArticle, createArticle, patchArticle, deleteArticle }
--- api/main.js
+++ api/main.js
@@ -1,51 +1,50 @@
-import { articleUrl, getArticleList, getArticle, createArticle, patchArticle, deleteArticle } from "./articleService.js";
+import * as articleService from "./articleService.js";
+import * as productService from "./productService.js";
 
-import { productUrl, getProductList, getProduct, createProduct, patchProduct, deleteProduct } from "./productService.js";
+//잘 동작하는지 테스트하기 위해 전부 console.log로 출력해서 확인해보고 있는 상태.
 
 //게시글 목록 조회 호출
-console.log(await getArticleList({
+console.log(await articleService.getArticleList({
     page: 1, 
     pageSize: 10, 
     keyword: "안주"
 }));
 
 //게시글 상세 조회 호출
-console.log(await getArticle(1002));
+console.log(await articleService.getArticle(1003));
 
 //게시글 등록 호출
-console.log(await createArticle({
+console.log(await articleService.createArticle({
     title: "테스트 타이틀",
     content: "테스트 콘텐트",
     image: "테스트 이미지"
 }));
 
 //게시글 수정 호출
-console.log(await patchArticle({
-    id: 1002,
+console.log(await articleService.patchArticle({
+    id: 1003,
     title: "테스트 타이틀11",
     content: "테스트 콘텐트12",
     image: "테스트 이미지11"
 }));
 
-console.log(await getArticle(1002));
-
 //게시글 삭제 호출
-console.log(await deleteArticle(1124));
+console.log(articleService.deleteArticle(996));
 
 
 
 //상품 목록 조회 호출
-console.log(await getProductList({
+console.log(await productService.getProductList({
     page: 1, 
     pageSize: 50, 
     keyword: "초콜릿"
 }));
 
 //상품 상세 조회 호출
-console.log(await getProduct(554));
+console.log(await productService.getProduct(553));
 
 //상품 등록 호출
-console.log(await createProduct({
+console.log(await productService.createProduct({
     name: "고려은단 비타민",
     description: "고려은단 비타민c 1000",
     price: 5000,
@@ -54,8 +53,8 @@ console.log(await createProduct({
 }));
 
 //상품 수정 호출
-console.log(await patchProduct({
-    id: 554,
+console.log(await productService.patchProduct({
+    id: 553,
     name: "고려은단 비타민",
     description: "고려은단 비타민c 1000",
     price: 5000,
@@ -63,6 +62,5 @@ console.log(await patchProduct({
     images: ["테스트 이미지"]
 }));
 
-
 //상품 삭제 호출
-console.log(await deleteProduct(557));
+console.log(await productService.deleteProduct(686));
