dddad92a263a5379fb214d9bdaeb7f01ec879592
민지영 <kuck523@gmail.com>
2025-01-07 09:34:09 +0900

    add: common button

---
+++ mission05/src/common/Button/index.jsx
@@ -0,0 +1,22 @@
+import { styled } from "styled-components";
+
+export default function Button({ children }) {
+  return (
+    <>
+      <CommonBTN>children</CommonBTN>
+    </>
+  );
+}
+
+const CommonBTN = styled.button`
+  background-color: #3692ff;
+  color: "#F3F4F6";
+  font-size: 1rem;
+  font-weight: 600;
+  line-height: 26px;
+
+  border: none;
+  border-radius: 8px;
+
+  padding: 8px 23px;
+`;
