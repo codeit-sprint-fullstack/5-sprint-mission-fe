dc069c6d1c442764c346b56e18af163416111066
민지영 <kuck523@gmail.com>
2025-01-07 09:54:26 +0900

    add: Likes

---
+++ mission05/src/common/Likes/index.jsx
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+import imgHeart from "../../assets/img/likes/ic_heart.png";
+
+export default function Likes({ likes }) {
+  return (
+    <CommonLikes>
+      <ImgHeart src={imgHeart} alt="좋아요의 하트 이미지" />
+      <p>{likes}</p>
+    </CommonLikes>
+  );
+}
+
+const CommonLikes = styled.div`
+  display: flex;
+  align-items: center;
+
+  color: #4b5563;
+  font-size: 0.75rem;
+  font-weight: 500;
+  line-height: 18px;
+`;
+
+const ImgHeart = styled.img`
+  width: 16px;
+  height: 16px;
+`;
