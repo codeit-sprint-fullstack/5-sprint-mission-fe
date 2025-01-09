4ffbdc3cd19806cfd2412254fbb0c765b8dfb85d
민지영 <kuck523@gmail.com>
2025-01-10 00:25:50 +0900

    add: Header-2

--- mission05/src/common/layouts/Header-2/index.jsx
+++ mission05/src/common/layouts/Header-2/index.jsx
@@ -1,10 +1,21 @@
 import Button from "../../components/Button";
+import logoImage from "../../../assets/logo/logo-img.png";
+import logoTitle from "../../../assets/logo/logo-title.png";
 
 export default function Header() {
   return (
-    <header>
-      <nav></nav>
-      <Button>로그인</Button>
+    <header className="w-full bg-white border-b border-gray-300 sticky top-0 z-10">
+      <div className="w-full max-w-screen-xl mx-auto flex justify-between px-6 py-2 md:px-4">
+        <section className="flex gap-6 tb:gap-5 md:gap-2">
+          <div className="flex items-center gap-2">
+            <img src={logoImage} alt="판다마켓" className="w-10 h-10" />
+            <img src={logoTitle} alt="판다마켓" className="w-auto h-9" />
+          </div>
+          <nav></nav>
+        </section>
+
+        <Button>로그인</Button>
+      </div>
     </header>
   );
 }
