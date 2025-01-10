51c803aa36b2fe5db535deae9a6c7271eb972035
HoeunRyu <hoeun9565@gmail.com>
2025-01-11 00:39:48 +0900

    [index.html]refact/메타태그 추가

--- sprint-mission/src/shared/store/useScreenSizeStore.jsx
+++ sprint-mission/src/shared/store/useScreenSizeStore.jsx
@@ -32,7 +32,7 @@ export const useScreenSizeStore = create((set, get) => ({
   },
 }));
 
-//TODO: 파일이름뭘로하지. 스토어랑 훅이랑 분리해야되나 고민~
+//파일이름뭘로하지. 스토어랑 훅이랑 분리해야되나 고민~
 //스토어에서 변경된 스크린사이즈 불러오기
 export const useMediaQuery = () =>
   useScreenSizeStore((state) => state.screenSize);
