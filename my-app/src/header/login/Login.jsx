18af231198240614abdb265a81997d9210d13ee8
minho <yiz4243@gmail.com>
2025-01-17 10:05:09 +0900

    add : main 초기화

--- my-app/src/header/login/Login.jsx
+++
@@ -1,9 +0,0 @@
-import "./login.css";
-export function Login() {
-  return (
-    <div id="login">
-      <button className="loginButton">로그인</button>
-      <img className="loginImg" src="/img/img_login.png" alt="" />
-    </div>
-  );
-}
