86ee52eb6fbfdc99ddf7bec3cec565aa42642f44
Syyga <gsh2672@gmail.com>
2025-02-24 15:48:13 +0900

    게시글 UI 생성

--- pages/components/articleList.js
+++ pages/components/articleList.js
@@ -11,7 +11,7 @@ export default function ArticleList() {
   }, []);
   return (
     <div className="w-full max-w-[1200px] py-8 px-4">
-      <div className="mt-6 flex justify-between ">
+      <div className="flex justify-between ">
         <div className="text-2xl font-bold mb-6">게시글</div>
         <Link href="/articles/new">
           <button className="bg-blue-500 text-white px-4 py-2 rounded-lg shadow">
@@ -29,10 +29,10 @@ export default function ArticleList() {
 
       <div className="space-y-4">
         {articles.map((article) => (
-          <div key={article._id} className="p-2">
-            <div className="flex justify-between">
-              <h3 className="text-lg font-semibold mb-2">{article.title}</h3>
-              <div className="flex justify-center items-center w-[72px] h-[72px] border rounded-[8px] object-cover">
+          <div key={article._id} className="  bg-[#fcfcfc] mb-6 border-b">
+            <div className="flex justify-between mb-4">
+              <div className="text-lg font-semibold mb-2">{article.title}</div>
+              <div className="flex justify-center items-center w-[72px] h-[72px] border rounded-[8px] object-cover bg-white">
                 <img
                   src={article.image}
                   className="w-[48px] h-[48px] object-cover "
@@ -41,12 +41,12 @@ export default function ArticleList() {
                 <div className="flex items-center space-x-1 text-gray-600"></div>
               </div>
             </div>
-            <div className="flex justify-between">
+            <div className="flex justify-between mb-6">
               <div className="text-sm text-gray-500">
                 {article.username} ·{" "}
                 {new Date(article.createdAt).toLocaleDateString()}
               </div>
-              <div className="flex">
+              <div className="flex justify-between w-[82px] items-center">
                 <Image
                   src="/heart.png"
                   alt="heart icon"
