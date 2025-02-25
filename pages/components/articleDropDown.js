70acc0b655172d0d673fe2a269b34529dd116655
Syyga <gsh2672@gmail.com>
2025-02-25 10:24:15 +0900

    검색기능 추가

--- pages/components/articleDropDown.js
+++ pages/components/articleDropDown.js
@@ -1,6 +1,6 @@
 import { useState, useRef, useEffect } from "react";
 
-export default function ArticleDropdownMenu({ onEdit, onDelete }) {
+export default function ArticleDropdownMenu({ onSortChange }) {
   const [isOpen, setIsOpen] = useState(false);
   const dropdownRef = useRef(null);
 
@@ -14,40 +14,46 @@ export default function ArticleDropdownMenu({ onEdit, onDelete }) {
     document.addEventListener("mousedown", handleClickOutside);
     return () => document.removeEventListener("mousedown", handleClickOutside);
   }, []);
-  const handleEdit = () => {
-    onEdit();
+
+  const handleSortChange = (e) => {
+    onSortChange(e.target.value);
     setIsOpen(false);
   };
 
-  const handleDelete = () => {
-    onDelete();
-    setIsOpen(false);
-  };
   return (
-    <div className="relative inline-block" ref={dropdownRef}>
-      <img
-        src="/ic_kebab.png"
-        alt="메뉴 열기"
-        className="cursor-pointer w-6 h-6"
-        onClick={() => setIsOpen((prev) => !prev)}
-      />
+    <div className="inline-block" ref={dropdownRef}>
+      <div className="block sm:hidden">
+        <img
+          src="/btn_sort.png"
+          alt="메뉴 열기"
+          className="cursor-pointer w-[42px] h-[42px]"
+          onClick={() => setIsOpen((prev) => !prev)}
+        />
+        {isOpen && (
+          <div className="right-0 mt-2 w-32  bg-white rounded-md shadow-lg z-10">
+            <button
+              className="block px-4 py-2 w-full h-full hover:bg-gray-100"
+              onClick={() => handleSortChange({ target: { value: "latest" } })}
+            >
+              최신 순
+            </button>
+            <button
+              className="block px-4 py-2 w-full hover:bg-gray-100"
+              onClick={() => handleSortChange({ target: { value: "likes" } })}
+            >
+              좋아요 순
+            </button>
+          </div>
+        )}
+      </div>
 
-      {isOpen && (
-        <div className="absolute right-0 mt-2 w-32 bg-white rounded-md shadow-lg z-10">
-          <button
-            className="block px-4 py-2 w-full hover:bg-gray-100"
-            onClick={handleEdit}
-          >
-            최신 순
-          </button>
-          <button
-            className="block px-4 py-2 w-full hover:bg-gray-100"
-            onClick={handleDelete}
-          >
-            좋아요 순
-          </button>
-        </div>
-      )}
+      <select
+        className="hidden sm:block border border-gray-300 h-full rounded-md px-2 py-1 cursor-pointer"
+        onChange={handleSortChange}
+      >
+        <option value="latest">최신 순</option>
+        <option value="likes">좋아요 순</option>
+      </select>
     </div>
   );
 }
