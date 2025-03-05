90c8a6c86c683612a407c492fc87b54b02854c2b
Syyga <gsh2672@gmail.com>
2025-03-05 12:50:30 +0900

    123

--- pages/api/articles.js
+++ pages/api/articles.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8000/api";
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 export const fetchArticles = async () => {
   const response = await axios.get(`${API_URL}/articles`);
